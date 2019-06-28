005e76d75803e6c08aa283ea3c4a591943b8c0b2
ikngtty <ikngtty@gmail.com>
2019-06-29 03:03:34 +0900

    feat: ability to buy colas by coins

--- vendor/vendor.go
+++ vendor/vendor.go
@@ -1,13 +1,26 @@
 package vendor
 
 type VendingMachine struct {
-	beverage string
+	beverage  string
+	coinCount int
 }
 
 func New(beverage string) *VendingMachine {
-	return &VendingMachine{beverage: beverage}
+	return &VendingMachine{
+		beverage:  beverage,
+		coinCount: 0,
+	}
 }
 
 func (vm *VendingMachine) Push() string {
+	if vm.coinCount <= 0 {
+		return ""
+	}
+	vm.coinCount -= 1
 	return vm.beverage
+}
+
+func (vm *VendingMachine) Insert100Yen() {
+	vm.coinCount += 1
+	return
 }
--- vendor/vendor_test.go
+++ vendor/vendor_test.go
@@ -2,12 +2,98 @@ package vendor
 
 import "testing"
 
-func TestVendingMachine_Push(t *testing.T) {
-	const want = "Cola"
-	vm := New(want)
+func TestVendingMachine_noCoin(t *testing.T) {
+	const beverage = "Cola"
+	vm := New(beverage)
+
 	got := vm.Push()
+	const want = ""
+
 	if got != want {
 		t.Errorf("want: %s", want)
 		t.Errorf("got : %s", got)
+	}
+}
+
+func TestVendingMachine_1Coin(t *testing.T) {
+	const beverage = "Cola"
+	vm := New(beverage)
+
+	gots := make([]string, 3)
+	wants := make([]string, 3)
+
+	vm.Insert100Yen()
+
+	gots[0] = vm.Push()
+	wants[0] = beverage
+	gots[1] = vm.Push()
+	wants[1] = ""
+	gots[2] = vm.Push()
+	wants[2] = ""
+
+	for i := range gots {
+		if gots[i] != wants[i] {
+			t.Errorf("wants[%d]: %s", i, wants[i])
+			t.Errorf("gots [%d]: %s", i, gots[i])
+		}
+	}
+}
+
+func TestVendingMachine_nCoins(t *testing.T) {
+	const beverage = "Cola"
+	vm := New(beverage)
+
+	gots := make([]string, 14)
+	wants := make([]string, 14)
+
+	vm.Insert100Yen()
+	vm.Insert100Yen()
+	vm.Insert100Yen()
+
+	gots[0] = vm.Push()
+	wants[0] = beverage
+	gots[1] = vm.Push()
+	wants[1] = beverage
+
+	vm.Insert100Yen()
+	vm.Insert100Yen()
+	vm.Insert100Yen()
+	vm.Insert100Yen()
+	vm.Insert100Yen()
+
+	gots[2] = vm.Push()
+	wants[2] = beverage
+	gots[3] = vm.Push()
+	wants[3] = beverage
+	gots[4] = vm.Push()
+	wants[4] = beverage
+	gots[5] = vm.Push()
+	wants[5] = beverage
+	gots[6] = vm.Push()
+	wants[6] = beverage
+	gots[7] = vm.Push()
+	wants[7] = beverage
+	gots[8] = vm.Push()
+	wants[8] = ""
+	gots[9] = vm.Push()
+	wants[9] = ""
+
+	vm.Insert100Yen()
+	vm.Insert100Yen()
+
+	gots[10] = vm.Push()
+	wants[10] = beverage
+	gots[11] = vm.Push()
+	wants[11] = beverage
+	gots[12] = vm.Push()
+	wants[12] = ""
+	gots[13] = vm.Push()
+	wants[13] = ""
+
+	for i := range gots {
+		if gots[i] != wants[i] {
+			t.Errorf("wants[%d]: %s", i, wants[i])
+			t.Errorf("gots [%d]: %s", i, gots[i])
+		}
 	}
 }
