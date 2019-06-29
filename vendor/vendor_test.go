c9852603e4bec7c81b009deb0408fc4c20b6bb70
ikngtty <ikngtty@gmail.com>
2019-06-29 22:47:07 +0900

    [ci skip] test: remove operation repetition 2/?

--- vendor/vendor_test.go
+++ vendor/vendor_test.go
@@ -5,104 +5,83 @@ import (
 	"testing"
 )
 
-func TestVendingMachine_noCoin(t *testing.T) {
-	log.Print("no coin")
-
+func TestVendingMachine(t *testing.T) {
 	const beverage = "Cola"
-	vm := New(beverage)
 
-	got := vm.Push()
-	const want = ""
-
-	if got != want {
-		t.Errorf("want: %s", want)
-		t.Errorf("got : %s", got)
+	type operation struct {
+		insertCount int
+		wants       []string
 	}
-}
-
-func TestVendingMachine_1Coin(t *testing.T) {
-	log.Print("1 coin")
-
-	const beverage = "Cola"
-	vm := New(beverage)
-
-	gots := make([]string, 3)
-	wants := make([]string, 3)
-
-	vm.Insert100Yen()
-
-	gots[0] = vm.Push()
-	wants[0] = beverage
-	gots[1] = vm.Push()
-	wants[1] = ""
-	gots[2] = vm.Push()
-	wants[2] = ""
-
-	for i := range gots {
-		if gots[i] != wants[i] {
-			t.Errorf("wants[%d]: %s", i, wants[i])
-			t.Errorf("gots [%d]: %s", i, gots[i])
-		}
+	testcases := []struct {
+		name       string
+		operations []operation
+	}{
+		{
+			name: "no coin",
+			operations: []operation{
+				{
+					insertCount: 0,
+					wants:       []string{""},
+				},
+			},
+		},
+		{
+			name: "1 coin",
+			operations: []operation{
+				{
+					insertCount: 1,
+					wants:       []string{beverage, "", ""},
+				},
+			},
+		},
+		{
+			name: "n coins",
+			operations: []operation{
+				{
+					insertCount: 3,
+					wants: []string{
+						beverage, beverage,
+					},
+				},
+				{
+					insertCount: 5,
+					wants: []string{
+						beverage, beverage, beverage, beverage,
+						beverage, beverage, "", "",
+					},
+				},
+				{
+					insertCount: 2,
+					wants: []string{
+						beverage, beverage, "", "",
+					},
+				},
+			},
+		},
 	}
-}
 
-func TestVendingMachine_nCoins(t *testing.T) {
-	log.Print("n coins")
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			log.Print(tc.name)
 
-	const beverage = "Cola"
-	vm := New(beverage)
+			vm := New(beverage)
+			pushCount := 0
 
-	gots := make([]string, 14)
-	wants := make([]string, 14)
+			for _, ope := range tc.operations {
+				for i := 0; i < ope.insertCount; i++ {
+					vm.Insert100Yen()
+				}
 
-	vm.Insert100Yen()
-	vm.Insert100Yen()
-	vm.Insert100Yen()
+				for _, want := range ope.wants {
+					got := vm.Push()
+					if got != want {
+						t.Errorf("wants[%d]: %s", pushCount, want)
+						t.Errorf("gots [%d]: %s", pushCount, got)
+					}
 
-	gots[0] = vm.Push()
-	wants[0] = beverage
-	gots[1] = vm.Push()
-	wants[1] = beverage
-
-	vm.Insert100Yen()
-	vm.Insert100Yen()
-	vm.Insert100Yen()
-	vm.Insert100Yen()
-	vm.Insert100Yen()
-
-	gots[2] = vm.Push()
-	wants[2] = beverage
-	gots[3] = vm.Push()
-	wants[3] = beverage
-	gots[4] = vm.Push()
-	wants[4] = beverage
-	gots[5] = vm.Push()
-	wants[5] = beverage
-	gots[6] = vm.Push()
-	wants[6] = beverage
-	gots[7] = vm.Push()
-	wants[7] = beverage
-	gots[8] = vm.Push()
-	wants[8] = ""
-	gots[9] = vm.Push()
-	wants[9] = ""
-
-	vm.Insert100Yen()
-	vm.Insert100Yen()
-
-	gots[10] = vm.Push()
-	wants[10] = beverage
-	gots[11] = vm.Push()
-	wants[11] = beverage
-	gots[12] = vm.Push()
-	wants[12] = ""
-	gots[13] = vm.Push()
-	wants[13] = ""
-
-	for i := range gots {
-		if gots[i] != wants[i] {
-			t.Errorf("wants[%d]: %s", i, wants[i])
-			t.Errorf("gots [%d]: %s", i, gots[i])
-		}
+					pushCount++
+				}
+			}
+		})
 	}
 }
